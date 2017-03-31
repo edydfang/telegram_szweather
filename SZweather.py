924d97612462484dd328ad9713b0819dcc038ffa
Edward <68852874@qq.com>
2017-03-31 14:37:24 +0800

    add requirements; reformat

--- SZweather.py
+++ SZweather.py
@@ -1,37 +1,46 @@
-#code: utf-8
-import urllib.request,json
-from telegram.ext import Updater,CommandHandler,MessageHandler, Filters
+# code: utf-8
+import urllib.request, json, logging
+from telegram.ext import Updater, CommandHandler  # MessageHandler, Filters
 
-updater = Updater(token='274017590:AAEvwhUM_MvyMio6Ux8Xdm68ho89RJ1tqwc')
-dispatcher = updater.dispatcher
 
-def sustech_weather():
-	bot.sendMessage(chat_id=update.message.chat_id, text=getweather())
+def main():
+    updater = Updater(token='274017590:AAEvwhUM_MvyMio6Ux8Xdm68ho89RJ1tqwc')
+    dispatcher = updater.dispatcher
+    szweather_handler = CommandHandler('sustech_weather', sustech_weather)
+    dispatcher.add_handler(szweather_handler)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    updater.start_polling()
+
+
+def sustech_weather(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="daf")  # getweather()
+
 
 # get last location and print weather
 def weather(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-szweather_handler = CommandHandler('sustech_weather', sustech_weather)
-dispatcher.add_handler(szweather_handler)
-updater.start_polling()
 
 # parameter to be changed
-def getweather(location):
-	url = 'http://wx.szmb.gov.cn/WeChat/map?lng=113.993088&lat=22.603228'
-	response = request.urlopen(url)
-	data = response.read()      # a `bytes` object
-	text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
-	info = json.loads(text)
-	# output
-	output = 'Data Time & Site Location: \n' + info['site'] + '\n'
-	output += 'Temperature: ' + info['temp'] + '\n'
-	output += 'WindSpeed:　' + info['wind'] + '\n'
-	output += 'Humidity:　' + info['hum'] + '\n'
-	output += 'Rain within 1 Hour:'+ info['rain1H'] + '\n'
-	output += 'Rain within 24 Hours: ' + info['rain24H'] + '\n'
-	if info['forcast']==[]:
-		output += 'No rain within 2 Hours.\n'
-	else:
-		output += '\n'.join(info['forcast'])
-	return output
+def getweather(location=None):
+    url = 'http://wx.szmb.gov.cn/WeChat/map?lng=113.993088&lat=22.603228'
+    response = urllib.request.urlopen(url)
+    data = response.read()  # a `bytes` object
+    text = data.decode('utf-8')  # a `str`; this step can't be used if data is binary
+    info = json.loads(text)
+    # output
+    output = 'Data Time & Site Location: \n' + info['site'] + '\n'
+    output += 'Temperature: ' + info['temp'] + '\n'
+    output += 'WindSpeed:　' + info['wind'] + '\n'
+    output += 'Humidity:　' + info['hum'] + '\n'
+    output += 'Rain within 1 Hour:' + info['rain1H'] + '\n'
+    output += 'Rain within 24 Hours: ' + info['rain24H'] + '\n'
+    if info['forcast'] == []:
+        output += 'No rain within 2 Hours.\n'
+    else:
+        output += '\n'.join(info['forcast'])
+    return output
+
+
+if __name__ == '__main__':
+    main()
