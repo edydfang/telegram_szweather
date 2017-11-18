c69139c08b8280425513e748daedc07b44e7bbda
edwardfang <fangyd1997@outlook.com>
2017-11-19 02:10:26 +0800

    ok to run for sustech

--- SZweather.py
+++ SZweather.py
@@ -1,10 +1,12 @@
-# code: utf-8
+#!/usr/bin/env python3
+#  code: utf-8
 import urllib.request, json, logging
 from telegram.ext import Updater, CommandHandler  # MessageHandler, Filters
 
 
+
 def main():
-    updater = Updater(token='274017590:AAG0RbxIVTV62ZQpFRwMwIBgZGH_GICjlZE')
+    updater = Updater(token='274017590:AAHwfPetzzkUdfdUtK--L1M793bj2TiwRbk')
     dispatcher = updater.dispatcher
     szweather_handler = CommandHandler('sustech_weather', sustech_weather)
     dispatcher.add_handler(szweather_handler)
@@ -13,7 +15,7 @@ def main():
 
 
 def sustech_weather(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="daf")  # getweather()
+    bot.sendMessage(chat_id=update.message.chat_id, text=getweather())  # getweather()
 
 
 # get last location and print weather
@@ -24,22 +26,25 @@ def weather(bot, update):
 # parameter to be changed
 def getweather(location=None):
     # new api js example http://wx.szmb.gov.cn/MobileWeather/script/nadforecastv1.js
-    url = 'http://wx.szmb.gov.cn/WeChat/map?lng=113.993088&lat=22.603228'
-    response = urllib.request.urlopen(url)
-    data = response.read()  # a `bytes` object
-    text = data.decode('utf-8')  # a `str`; this step can't be used if data is binary
-    info = json.loads(text)
-    # output
-    output = 'Data Time & Site Location: \n' + info['site'] + '\n'
-    output += 'Temperature: ' + info['temp'] + '\n'
-    output += 'WindSpeed:　' + info['wind'] + '\n'
-    output += 'Humidity:　' + info['hum'] + '\n'
-    output += 'Rain within 1 Hour:' + info['rain1H'] + '\n'
-    output += 'Rain within 24 Hours: ' + info['rain24H'] + '\n'
-    if info['forcast'] == []:
-        output += 'No rain within 2 Hours.\n'
-    else:
-        output += '\n'.join(info['forcast'])
+    url = 'http://wx.szmb.gov.cn/MobileWeather/position/query?latitude=22.597700&longitude=114.000110'
+    output = ""
+    try:
+        response = urllib.request.urlopen(url)
+        data = response.read()  # a `bytes` object
+        text = data.decode('utf-8')  # a `str`; this step can't be used if data is binary
+        info = json.loads(text)
+        # output
+        output = 'Update Time %s\n' % info['dataTimeFormat']
+        output = 'Site Location: %s\n' % info['content']
+        output += 'Forcast: %s\n' % info['forecast']
+        output += 'Tempature Now: %s\n' % info['temp']
+        output += 'Humidity: %s%%\n' % info['humidity']
+        output += 'Rain within 1 Hour: %s\n' % info['r01h']
+        output += 'Rain within 3 Hours: %s\n' % info['r03h']
+        output += 'Wind: %s\n' % info['windStr']
+    except KeyError:
+        logging.info("Weather querying failed.")
+        output = "Sorry, weather querying failed."
     return output
 
 
