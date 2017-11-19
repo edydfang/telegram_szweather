81cea69c2d92e1c587e27f1e2dcb95853ec29b82
edwardfang <fangyd1997@outlook.com>
2017-11-19 14:00:50 +0800

    add weather forecast

--- SZweather.py
+++ SZweather.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 #  code: utf-8
 import urllib.request, json, logging
+import urllib.error
 import sys
 from telegram.ext import Updater, CommandHandler  # MessageHandler, Filters
 
-
+DEBUG_MODE = False
 
 def main():
     TOKEN = "274017590:AAHwfPetzzkUdfdUtK--L1M793bj2TiwRbk"
@@ -12,23 +13,40 @@ def main():
     logging.info("Start Servering...")
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
-    szweather_handler = CommandHandler('sustech_weather', sustech_weather)
-    dispatcher.add_handler(szweather_handler)
+    weather_handler = CommandHandler('sustech_weather', sustech_weather)
+    forcast_handler = CommandHandler('sustech_forecast', sustech_forecast)
+    dispatcher.add_handler(weather_handler)
+    dispatcher.add_handler(forcast_handler)
     logging.info("Start Polling...")
-    #updater.start_polling()
-    '''
-    just for heroku
-    '''
-    import os
-    PORT = int(os.environ.get('PORT', '5000'))
-    # add handlers
-    updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path=TOKEN)
-    updater.bot.set_webhook("https://telegram-weather.herokuapp.com/" + TOKEN)
-    updater.idle()
+    if DEBUG_MODE:
+        updater.start_polling()
+    else:
+        '''
+        just for heroku
+       '''
+        import os
+        PORT = int(os.environ.get('PORT', '5000'))
+        # add handlers
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        updater.bot.set_webhook("https://telegram-weather.herokuapp.com/" + TOKEN)
+        updater.idle()
     # end heroku
 
+
+def sustech_forecast(bot, update):
+    logging.info("Received command.")
+    bot.sendMessage(chat_id=update.message.chat_id, text=getforecast())  # getweather()
+    logging.info("Send successfully.")
+
+def get_API_data(location=(22.597700, 114.000110)):
+    url = 'http://wx.szmb.gov.cn/MobileWeather/position/query?latitude=%.6f&longitude=%.6f' % location
+    response = urllib.request.urlopen(url)
+    data = response.read()  # a `bytes` object
+    text = data.decode('utf-8')  # a `str`; this step can't be used if data is binary
+    info = json.loads(text)
+    return info
 
 def sustech_weather(bot, update):
     logging.info("Received command.")
@@ -41,25 +59,37 @@ def weather(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
-# parameter to be changed
-def getweather(location=None):
+def getweather(location=(22.597700, 114.000110)):
     # new api js example http://wx.szmb.gov.cn/MobileWeather/script/nadforecastv1.js
-    url = 'http://wx.szmb.gov.cn/MobileWeather/position/query?latitude=22.597700&longitude=114.000110'
-    output = ""
+    info = get_API_data(location)
     try:
-        response = urllib.request.urlopen(url)
-        data = response.read()  # a `bytes` object
-        text = data.decode('utf-8')  # a `str`; this step can't be used if data is binary
-        info = json.loads(text)
         # output
         output = 'Update Time %s\n' % info['dataTimeFormat']
-        output = 'Site Location: %s\n' % info['content']
-        output += 'Forcast: %s\n' % info['forecast']
-        output += 'Tempature Now: %s\n' % info['temp']
+        output += 'Site Location: %s\n' % info['content']
+        output += 'Weather Now: %s\n' % info['forecast']
+        output += u'Tempature Now: %s\u2103\n' % info['temp']
         output += 'Humidity: %s%%\n' % info['humidity']
-        output += 'Rain within 1 Hour: %s\n' % info['r01h']
-        output += 'Rain within 3 Hours: %s\n' % info['r03h']
+        output += 'Rain within 1 Hour: %s mm\n' % info['r01h']
+        output += 'Rain within 3 Hours: %s mm\n' % info['r03h']
         output += 'Wind: %s\n' % info['windStr']
+    except (KeyError,urllib.error.HTTPError):
+        logging.info("Weather querying failed.")
+        output = "Sorry, weather querying failed."
+
+
+    return output
+
+# parameter to be changed
+def getforecast(location=(22.597700, 114.000110)):
+    # new api js example http://wx.szmb.gov.cn/MobileWeather/script/nadforecastv1.js
+    info = get_API_data(location)
+    output = "Weather Forecast for next week:"
+    try:
+        # output
+        for day in info['week']:
+            newline = "[%s %s]\nTemp: %s~%s\u2103\nWeather:%s\n" \
+                      % (day['formatTime'], day['week'], day['minTemp'], day['maxTemp'], day['weather'])
+            output += newline
     except KeyError:
         logging.info("Weather querying failed.")
         output = "Sorry, weather querying failed."
